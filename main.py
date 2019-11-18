c9fdab2a8ed56b9716dfce1687878e0c3a7a71c0
JordanJose <JordanJose0410@gmail.com>
2019-11-17 22:37:47 -0400

    Modularizando a tela de rank e adicionando main para testes

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from rank_screen import *
+
+pygame.init()
+
+screen_resolution = (1000, 500)
+
+screen = pygame.display.set_mode(screen_resolution)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    
+    make_screen_rank(screen)
+    
+    pygame.display.flip()
--- rank_screen.py
+++ rank_screen.py
@@ -3,48 +3,42 @@ import ast
 
 pygame.init()
 
+# screen_resolution = (1000, 500)
+# clock_tick = 30
+# clock = pygame.time.Clock()
+
 # função para ler o arquivo
-def ranking(font_tam):
-    arq = open("content/rank.txt").readline()
+def ranking(text_tam, text_font, rank_arq, text_color):
+    arq = open("content/{}".format(rank_arq)).readline()
     dic_rank = ast.literal_eval(arq)
     cont = len(dic_rank)
     list_ranking = []
-    font = pygame.font.SysFont("ubuntumono", font_tam)
+    font = pygame.font.SysFont(text_font, text_tam)
     if cont:
         for player in sorted(dic_rank, key = dic_rank.get):
             text = font.render("{}. {} - {} points".format(cont, player,
-                                                           dic_rank[player]), True, (255, 255, 250))
+                                                           dic_rank[player]), True, text_color)
             list_ranking.append(text)
             cont-=1
     return list_ranking
 
-def write_rank():
-    global screen
-    list_texts = ranking(40)
+def write_rank(screen, text_tam, text_font, rank_arq, text_color):
+    list_texts = ranking(text_tam, text_font, rank_arq, text_color)
     x_init = 250
     y_init = 0
     for text in list_texts[::-1]:
         screen.blit(text, (x_init, y_init))
         y_init += text.get_height() + 10
 
-screen_resolution = (1000, 500)
-clock_tick = 30
 
-bg = pygame.image.load("content/nebula.jpg")
-
-screen = pygame.display.set_mode(screen_resolution)
-
-clock = pygame.time.Clock()
-
-pygame.display.set_caption("Rank")
-
-while True:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            pygame.quit()
-
-    screen.blit(bg, (0,0))
-    write_rank()
-    
-    pygame.display.flip()
-    clock.tick(clock_tick)
+def make_screen_rank(
+    screen,
+    text_font="ubuntumono", 
+    text_tam = 38, 
+    background = "nebula.jpg",
+    text_color = (255, 255, 255),
+    rank_arq = "rank.txt"):
+    pygame.display.set_caption("Rank")
+    bg = pygame.image.load("content/{}".format(background))
+    screen.blit(bg, (0, 0))
+    write_rank(screen, text_tam, text_font, rank_arq, text_color)
