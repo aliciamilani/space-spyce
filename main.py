c98e42d645332da91203c77cedaaa670cb03f9aa
JordanJose <JordanJose0410@gmail.com>
2019-11-18 20:47:20 -0400

    adicionando versao para testar a nave

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
-from rank_screen import *
+from rank_screen import make_screen_rank
+from testes_inimigo_2 import *
 
 pygame.init()
+clock = pygame.time.Clock()
 
 screen_resolution = (1000, 500)
 
 screen = pygame.display.set_mode(screen_resolution)
 
+ovnis = []
+ovnis.append(make_ovni())
+ovni_sprite = pygame.image.load("content/nave_teste.png")
+
 while True:
+
+    clock.tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
     
     make_screen_rank(screen)
-    
+    update_ovni(screen, ovni_sprite, ovnis)
     pygame.display.flip()
---
+++ testes_inimigo_2.py
@@ -0,0 +1,20 @@
+import pygame
+from random import randint, choice, random
+
+def make_ovni(speed = 1):
+    vec = pygame.math.Vector2
+    ovni_info = {}
+    ovni_info['vec_init'] = vec(1020, randint(0, 500))
+    ovni_info['vec_mov'] = vec(((-1)-speed), randint(-2,2)-(speed*random()))
+    ovni_info['up'] = True
+    return ovni_info
+
+def update_ovni(screen, sprite, ovni_list):
+    for ovni in ovni_list:
+        if(ovni['up']):
+            ovni['vec_init'] += ovni['vec_mov']
+            if(ovni['vec_init'].y < 100 or ovni['vec_init'].y > 450):
+                ovni['vec_mov'].y *= -1
+            screen.blit(sprite, ovni['vec_init'])
+        else:
+            ovni_list.remove(ovni)
