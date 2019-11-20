634a106f765ad5a5b7606378cb1e6e86892fdfd4
JordanJose <JordanJose0410@gmail.com>
2019-11-20 16:10:14 -0400

    Versao de testes dos tiros dos ets

--- enemy_2.py
+++ enemy_2.py
@@ -10,8 +10,26 @@ def make_ovni(speed=1):
     ovni_info['up'] = True
     return ovni_info
 
+def make_shot(begin, end):
+    vec = pygame.math.Vector2
+    shot_info = {}
+    shot_info['vec_init'] = begin
+    shot_info['vec_mov'] = vec.normalize((end - begin))
+    shot_info['up'] = True
+    return shot_info
 
-def update_ovni(screen, sprite, ovni_list):
+def updade_shot(screen, sprite, shot_list, speed):
+    for shot in shot_list:
+        if shot['vec_init'].x < 10:
+            shot['up'] = False
+        if shot['up']:
+            shot['vec_init'] += shot['vec_mov']*speed
+            screen.blit(sprite, shot['vec_init'])
+        else:
+            shot_list.remove(shot)
+
+def update_ovni(screen, sprite, ovni_list, shot_list, x_nave, y_nave):
+    vec = pygame.math.Vector2
     for ovni in ovni_list:
         if ovni['vec_init'].x < 10:
             ovni['up'] = False
@@ -21,6 +39,10 @@ def update_ovni(screen, sprite, ovni_list):
             if(ovni['vec_init'].y < 10 or ovni['vec_init'].y > 450):
                 ovni['vec_mov'].y *= -1
             screen.blit(sprite, ovni['vec_init'])
+
+            if(random() < 0.001):
+                begin = vec(ovni['vec_init'].x, ovni['vec_init'].y)
+                shot_list.append(make_shot(begin ,vec(x_nave, y_nave)))
 
         else:
             ovni_list.remove(ovni)
--- gameplay.py
+++ gameplay.py
@@ -9,11 +9,14 @@ def make_screen_game(screen):
     x_nave = 30
     y_nave = 200
     ovni_sprite = pygame.image.load("content/nave_teste.png")
+    shot_ovni_sprite = pygame.image.load("content/bullet.png")
+    shot_ovni_sprite = pygame.transform.rotate(shot_ovni_sprite, -180)
     background = "nebula.jpg"
     pygame.display.set_caption("Gameplay")
     bg = pygame.image.load("content/{}".format(background))
 
     ovnis = []
+    ovnis_shot = []
     spawn_chance = 0.04  # Chance de spawnar um ET
 
     shoot = []
@@ -25,8 +28,9 @@ def make_screen_game(screen):
         support.draw_nave(screen, x_nave, y_nave)
 
         if(random.random() < spawn_chance):
-            ovnis.append(enemy_2.make_ovni(3))
-        enemy_2.update_ovni(screen, ovni_sprite, ovnis)
+            ovnis.append(enemy_2.make_ovni(2))
+        enemy_2.update_ovni(screen, ovni_sprite, ovnis, ovnis_shot, x_nave, y_nave)
+        enemy_2.updade_shot(screen, shot_ovni_sprite, ovnis_shot, 3)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
