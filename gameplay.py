d3d3f2861172c68d53ac13cbfe2fdb0488d6d37a
JordanJose <JordanJose0410@gmail.com>
2019-11-22 19:56:23 -0400

    Adicionando mudanças na gameplay

--- gameplay.py
+++ gameplay.py
@@ -12,20 +12,19 @@ def make_screen_game(screen):
     y_nave = 200
     ovni_sprite = pygame.image.load("content/nave_teste.png")
     shot_ovni_sprite = pygame.image.load("content/circle.png")
-    shot_ovni_sprite = pygame.transform.rotate(shot_ovni_sprite, -180)
+    power_up_sprite = pygame.image.load("content/star.png")
 
     x_bg = 0
     x_bg_2 = 0
-    # shot_ovni_sprite = pygame.transform.rotate(shot_ovni_sprite, -180)
+    life = 0
     background = "nebula.jpg"
-    # bg_2 = pygame.image.load("content/stars.png")
-    # pygame.display.set_caption("Gameplay")
     bg = pygame.image.load("content/{}".format(background))
 
     ovnis = []
     ovnis_shot = []
-    spawn_chance = 0.01  # Chance de spawnar um ET
-
+    spawn_chance_et = 0.01  # Chance de spawnar um ET
+    list_power_up = []
+    spawn_chance_life = 0.001 # Chance de spawnar um power up
     shoot = []
 
     game = True
@@ -37,10 +36,19 @@ def make_screen_game(screen):
                                            x_bg_2, 1994, 2, -300)
         support.draw_nave(screen, x_nave, y_nave)
 
-        if(random.random() < spawn_chance):
-            ovnis.append(enemy_2.make_ovni(1))
+        if(random.random() < spawn_chance_et):
+            ovnis.append(enemy_2.make_ovni(2))
+        if(random.random() < spawn_chance_life):
+            list_power_up.append(support.make_life_up(10))
+
+        power_up = support.update_life_up(screen, power_up_sprite, list_power_up, x_nave, y_nave)
+
+        if power_up:
+            life += 1
+            print(life)
+
         enemy_2.update_ovni(screen, ovni_sprite, ovnis,
-                            ovnis_shot, x_nave, y_nave)
+                            ovnis_shot, 0.001,x_nave, y_nave)
         enemy_2.updade_shot(screen, shot_ovni_sprite, ovnis_shot, 2)
 
         damage_taken = support.colide_with_nave(x_nave, y_nave,
